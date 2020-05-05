d7c4bde4c1cad66ba3d478a44f099f8d7403e993
Victor Santiago <vsantiago113sec@gmail.com>
2020-05-05 16:56:12 -0500

    Update __init__.py

--- FiremonClient/__init__.py
+++ FiremonClient/__init__.py
@@ -12,32 +12,31 @@ class FiremonAuthError(Exception):
 
 
 class Client:
-    def __init__(self, verify=bool(), warnings=bool(), api_version='v1'):
+    def __init__(self, verify=False, headers=None, server=None, token=None):
         self.verify = bool(verify)
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) if warnings is False else None
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-        self.api_version = api_version
-        self.headers = {'Content-Type': 'application/json'}
-        self.server = None
-        self.token = None
-        self.base_url = None
-        self.username = None
-        self.password = None
+        if headers:
+            self.headers = headers
+        else:
+            if token:
+                self.headers = {'Content-Type': 'application/json', 'X-FM-AUTH-Token': token}
+            else:
+                self.headers = {'Content-Type': 'application/json'}
 
-    def connect(self, server=str(), username=str(), password=str()):
+        self.server = server
+        self.token = token
+
+    def connect(self, server='', username='', password=''):
         self.server = server.replace('https://', '').replace('http://', '').strip('/')
-        self.username = username
-        self.password = password
         url = ('https://{}/securitymanager/api/authentication/login'.format(server))
-        _response = requests.post(url, headers=self.headers, data=json.dumps({'username': username,
-                                                                              'password': password}),
-                                  verify=self.verify)
+        _response = requests.post(url, headers=self.headers,
+                                  data=json.dumps({'username': username, 'password': password}), verify=self.verify)
 
         if _response.status_code in [200]:
             token = _response.json().get('token')
+            self.headers = {'Content-Type': 'application/json', 'X-FM-AUTH-Token': token}
             self.token = token
-            self.base_url = 'https://{}/securitymanager/api'.format(server, self.api_version)
-            self.headers.update({'X-FM-AUTH-Token': token})
             return _response.status_code
         elif _response.status_code == 401:
             raise FiremonAuthError('Authentication Error!')
@@ -45,37 +44,37 @@ class Client:
             raise FiremonError('HTTP Error,  Status Code: {}'.format(_response.status_code))
 
     def close(self):
-        url = ('{}/{}'.format(self.base_url, 'authentication/logout'))
+        url = ('{}/{}'.format('https://{}/securitymanager/api'.format(self.server).strip('/'), 'authentication/logout'))
         __response = requests.post(url, headers=self.headers, verify=False)
         return __response.status_code
 
     def get(self, method=str(), **kwargs):
-        response = requests.get('{}/{}'.format(self.base_url.strip('/'), method), headers=self.headers,
-                                verify=self.verify, params=kwargs)
+        response = requests.get('{}/{}'.format('https://{}/securitymanager/api'.format(self.server).strip('/'), method),
+                                headers=self.headers, verify=self.verify, params=kwargs)
         if response.status_code in [200]:
             return response.json()
         else:
             raise FiremonError(response.status_code)
 
     def add(self, method: str, data: dict) -> dict:
-        response = requests.post('{}/{}'.format(self.base_url.strip('/'), method), headers=self.headers,
-                                 verify=self.verify, json=data)
+        response = requests.post('{}/{}'.format('https://{}/securitymanager/api'.format(self.server).strip('/'),
+                                                method), headers=self.headers, verify=self.verify, json=data)
         if response.status_code in [200]:
             return response.json()
         else:
             raise FiremonError(response.status_code)
 
     def update(self, method: str, data: dict) -> dict:
-        response = requests.put('{}/{}'.format(self.base_url.strip('/'), method), headers=self.headers,
-                                verify=self.verify, json=data)
+        response = requests.put('{}/{}'.format('https://{}/securitymanager/api'.format(self.server).strip('/'), method),
+                                headers=self.headers, verify=self.verify, json=data)
         if response.status_code in [200]:
             return response.json()
         else:
             raise FiremonError(response.status_code)
 
     def delete(self, method: str) -> dict:
-        response = requests.delete('{}/{}'.format(self.base_url.strip('/'), method), headers=self.headers,
-                                   verify=self.verify)
+        response = requests.delete('{}/{}'.format('https://{}/securitymanager/api'.format(self.server).strip('/'),
+                                                  method), headers=self.headers, verify=self.verify)
         if response.status_code in [200]:
             return response.json()
         else:
